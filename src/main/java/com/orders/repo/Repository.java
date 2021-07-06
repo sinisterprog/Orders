13b10949975b4f309681cd037e4ba817abfa9ab6
Mike <sinister_prog@hotmail.com>
2021-07-06 22:24:53 +0900

    Initial upload

--- src/main/java/com/orders/repo/Repository.java
+++ src/main/java/com/orders/repo/Repository.java
@@ -3,5 +3,6 @@ package com.orders.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Repository extends JpaRepository<OrderPersist, Long> {
+    OrderPersist getOrderById(Long id);
 
 }
